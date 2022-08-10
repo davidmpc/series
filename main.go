65f8c55589206f4813621bbda59fe79e52ed7594
David Ma <mpchust@163.com>
2022-08-11 02:44:22 +0800

    day2

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
+	"log"
 	"net/http"
 	"regexp"
 	"time"
 )
 
 var queryTimeout = flag.Duration("queryTimeout", time.Minute, "maximum amount of a query is allowed to process")
+var staticPath = flag.String("static", "static", "path to static data")
 
 type routeHandler func(parts []string, w http.ResponseWriter, req *http.Request)
 
@@ -51,5 +55,34 @@ func init() {
 }
 
 func mustEncode(status int, w http.ResponseWriter, ob interface{}) {
+	b, err := json.Marshal(ob)
+	if err != nil {
+		log.Fatalf("error encoding %v", ob)
+	}
+	w.WriteHeader(status)
+	w.Header().Set("Content-length", fmt.Sprintf("%d", len(b)))
+	w.Write(b)
+}
 
+func emitError(status int, w http.ResponseWriter, e, reason string) {
+	m := map[string]string{"error": e, "reason": reason}
+	mustEncode(status, w, m)
+}
+
+func staticHandler(parts []string, w http.ResponseWriter, req *http.Request) {
+	w.Header().Del("Content-type")
+	http.StripPrefix("/_static", http.FileServer(http.Dir(*staticPath))).ServeHTTP(w, req)
+}
+
+func reservedHandler(parts []string, w http.ResponseWriter, req *http.Request) {
+	emitError(400, w, "illegal_database_name", "must begin with a letter")
+}
+
+func defaultHandler(parts []string, w http.ResponseWriter, req *http.Request) {
+	emitError(400, w, "no_handler", fmt.Sprintf("Can't handle %v to %v\n", req.Method, req.URL.Path))
+}
+
+func optionsHandler(parts []string, w http.ResponseWriter, req *http.Request) {
+	methods := []string{}
+	for _, r :=
 }
