bab7bba251e07f557bd67f02cccca77c15ddd844
David Ma <mpchust@163.com>
2022-10-11 18:37:20 +0800

    add query

--- go.mod
+++ go.mod
@@ -3,14 +3,16 @@ module series
 go 1.17
 
 require (
+	github.com/dustin/go-jsonpointer v0.0.0-20160814072949-ba0abeacc3dc
 	github.com/dustin/gomemcached v0.0.0-20160817010731-a2284a01c143
 	github.com/dustin/yellow v0.0.0-20140729025043-2ce7236602be
 	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/gopacket v1.1.19
 	github.com/mschoch/gouchstore v0.0.0-20151012155121-ebc3bea732ff
 	github.com/mschoch/mergesort v0.0.0-20140321150500-46d4b13617d1 // indirect
 )
 
 require (
-	github.com/dustin/go-jsonpointer v0.0.0-20160814072949-ba0abeacc3dc // indirect
 	github.com/dustin/gojson v0.0.0-20160307161227-2e71ec9dd5ad // indirect
+	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
 )
--- query.go
+++ query.go
@@ -3,9 +3,14 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"github.com/dustin/go-jsonpointer"
+	"log"
+	"math"
+	"reflect"
+	"sync/atomic"
 	"time"
 
-	"github.com/dustin/go-jsonpointer"
 	"github.com/mschoch/gouchstore"
 )
 
@@ -62,23 +67,200 @@ type queryIn struct {
 	cherr      chan error
 }
 
-func resolveFetch(b []byte, keys []string) map[string]interface{} {
-	rv := map[string]interface{}{}
-	found, err := jsonpointer.FindMany(b, keys)
+func resolveFetch(doc []byte, keys []string) map[string]interface{} {
+	ret := map[string]interface{}{}
+	found, err := jsonpointer.FindMany(doc, keys)
 	if err != nil {
-		return rv
+		return ret
 	}
 	for k, v := range found {
 		var val interface{}
 		err = json.Unmarshal(v, &val)
-		if err == nil {
-			rv[k] = v
+		if err != nil {
+			ret[k] = val
 		}
 	}
-	return rv
+	return ret
 }
 
-func processDoc(di *gouchstore.DocumentInfo, chs []chan ptrval, doc []byte, ptrs []string, filters []string,
-	filtervals []string, included bool) {
+func processDoc(di *gouchstore.DocumentInfo, chs []chan ptrval, doc []byte,
+	ptrs []string, filters []string, filtervals []string, included bool) {
+	seen := map[string]bool{}
+	var keys []string
+	for _, p := range ptrs {
+		if !seen[p] {
+			seen[p] = true
+			keys = append(keys, p)
+		}
+	}
+	for _, f := range filters {
+		if !seen[f] {
+			seen[f] = true
+			keys = append(keys, f)
+		}
+	}
+	fetched := resolveFetch(doc, keys)
+	for i, p := range filters {
+		val := fetched[p]
+		checkval := filtervals[i]
+		switch val.(type) {
+		case string:
+			if val != checkval {
+				return
+			}
+		case int, uint, uint64, int64, float64:
+			v := fmt.Sprintf("%v", val)
+			if v != checkval {
+				return
+			}
+		default:
+			return
+		}
+	}
+	pv := ptrval{
+		di:       di,
+		val:      nil,
+		included: included,
+	}
+	for i, p := range ptrs {
+		val := fetched[p]
+		if p == "_id" {
+			val = di.ID
+		}
+		switch x := val.(type) {
+		case int, uint, int64, uint64, float64:
+			v := fmt.Sprintf("%v", x)
+			pv.val = v
+			chs[i] <- pv
+		default:
+			pv.val = x
+			chs[i] <- pv
+		}
+	}
+}
+
+func processDocs(pi *processIn) {
+	result := processOut{pi.key, nil, nil, pi.cacheKey, 0}
+
+	if len(pi.ptrs) == 0 {
+		log.Panicf("No pointers specified in query, %#v", pi)
+	}
+	db, err := dbopen(pi.dbname)
+	if err != nil {
+		result.err = err
+		pi.out <- &result
+		return
+	}
+	defer dbclose(db)
+
+	chans := make([]chan ptrval, 0, len(pi.ptrs))
+	resultchans := make([]chan interface{}, 0, len(pi.ptrs))
+	for i, r := range pi.reds {
+		chans[i] = make(chan ptrval)
+		resultchans[i] = make(chan interface{})
+
+		go func(fi int, fr string) {
+			resultchans[fi] <- reducers[fr](chans[fi])
+		}(i, r)
+	}
+
+	go func() {
+		closeAll := func(chans interface{}) {
+			v := reflect.ValueOf(chans)
+			for i, imax := 0, v.Len(); i < imax; i++ {
+				v.Index(i).Close()
+			}
+		}
+		defer closeAll(chans)
+		doDoc := func(di *gouchstore.DocumentInfo, included bool) {
+			doc, err := db.DocumentByDocumentInfo(di)
+			if err != nil {
+				for i := range pi.ptrs {
+					chans[i] <- ptrval{di, nil, included}
+				}
+			} else {
+				processDoc(di, chans, doc.Body, pi.ptrs, pi.filters, pi.filtervals, included)
+			}
+		}
+
+		for _, di := range pi.infos {
+			doDoc(di, true)
+		}
+		if pi.nextInfo != nil {
+			doDoc(pi.nextInfo, false)
+		}
+	}()
+
+	results := make([]interface{}, len(pi.ptrs))
+	for i := range pi.ptrs {
+		results[i] = <-resultchans[i]
+		if f, fok := results[i].(float64); fok && (math.IsNaN(f) || math.IsInf(f, 0)) {
+			results[i] = 0
+		}
+	}
+	result.value = results
+
+	if result.cacheOpaque == 0 && result.cacheKey != "" {
+		select {
+		case cacheInputSet <- &result:
+		default:
+		}
+	}
+	pi.out <- &result
+}
+
+func docProcessor(ch <-chan *processIn) {
+	for pi := range ch {
+		if time.Now().Before(pi.before) {
+			processDocs(pi)
+		} else {
+			pi.out <- &processOut{pi.key, nil, errTimeout, "", 0}
+		}
+	}
+}
+
+func fetchDocs(dbname string, key int64, infos []*gouchstore.DocumentInfo,
+	nextInfo *gouchstore.DocumentInfo, ptrs []string, reds []string, filters []string,
+	filtervals []string, before time.Time, out chan<- *processOut) {
+
+	i := processIn{infos, nextInfo, key, dbname, "", ptrs, reds, filters,
+		filtervals, before, out}
+	cacheInput <- &i
+}
+
+func runQuery(q *queryIn) {
+	if len(q.ptrs) == 0 {
+		q.cherr <- fmt.Errorf("at least one query is required")
+		return
+	}
+	if q.group == 0 {
+		q.cherr <- fmt.Errorf("group level can't be zero")
+		return
+	}
+
+	db, err := dbopen(q.dbname)
+	if err != nil {
+		log.Printf("error opening db: %v - %v", q.dbname, err)
+		q.cherr <- err
+		return
+	}
+	defer dbclose(db)
+
+	chunk := int64(time.Duration(q.group) * time.Millisecond)
+
+	info := []*gouchstore.DocumentInfo{}
+	g := int64(0)
+	nextg := ""
+
+	err = db.AllDocuments(q.from, q.to, func(db *gouchstore.Gouchstore, di *gouchstore.DocumentInfo,
+		userContext interface{}) error {
+		kstr := di.ID
+		var err error
+
+		atomic.AddInt32(&q.totalKeys, 1)
+	}, nil)
+}
+
+var reducers = map[string]reducer{
 
 }
